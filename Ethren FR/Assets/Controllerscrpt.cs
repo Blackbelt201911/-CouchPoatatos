658a0fd63436b6dcc8e48301f4ae173f9b55bcd9
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2023-08-24 17:14:30 -0500

    added a player controller

--- Ethren FR/Assets/Controllerscrpt.cs
+++ Ethren FR/Assets/Controllerscrpt.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Controllerscrpt : MonoBehaviour
 {
+    public float movespeed;
+    public Rigidbody2D rb;
+    private Vector2 MovementDiction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,22 @@ public class Controllerscrpt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Inputmanger();
     }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    void Inputmanger()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        MovementDiction = new Vector2(moveX, moveY);
+    } 
+
+    void Move()
+    {
+        rb.velocity = new Vector2(MovementDiction.x * movespeed, MovementDiction.y * movespeed); 
+    }
+
 }
