c4e36072194f6f38c3d8ce032dcf2bc777364a70
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2023-11-01 18:01:17 -0500

    atioms pain suffer

--- Ethren V 8/Assets/Scrpits/playercontorller.cs
+++ Ethren V 8/Assets/Scrpits/playercontorller.cs
@@ -4,50 +4,57 @@ using UnityEngine;
 
 public class playercontorller : MonoBehaviour
 {
-    //How fast you move
-    public int movespeed = 6;
-    
-    // for phicys and stuff
-    public Rigidbody2D rb;
-    
-    //Place to store movement it hasw nothing to do with dirtion i just called it that becasue
-    private Vector2 moveDirtion;
+    public Animator anim;
 
-    
- 
+    public float moveSpeed;
 
+    private Rigidbody2D rb;
 
+    private float x;
+    private float y;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 input;
+    private bool moving;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        GetInput();
+        Animate();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = input * moveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetInput()
     {
-        inputManger(); // runs inputmanger look below for the inputmanger code
-    }
-    
-    
-    
-    
-    private void FixedUpdate() // FixedUpdate is better for phiclys stuff like movemnet
-    {
-        Move();
-    }
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
 
-    void inputManger()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal"); //gets the X input
-        float moveY = Input.GetAxisRaw("Vertical"); //gets the Y input
-        moveDirtion = new Vector2(moveX, moveY).normalized; // gives moveDirtion teh power to be called for movement again it has nothing to do with dirtion it just stores where we  want our player to go. .noremalized makes the dignles the same speed as the others 
-    
+        input = new Vector2(x, y);
+        input.Normalize();
     }
-
-    void Move()
+    private void Animate()
     {
-        rb.velocity = new Vector2(moveDirtion.x * movespeed, moveDirtion.y * movespeed); //Changes the RB loction in scean to move. moveDirtion.x is kind of like a if statment and then we times that by move speed for the speed 
+        if (input.magnitude > 0.1f || input.magnitude < -0.1f)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+
+        if (moving)
+        {
+            anim.SetFloat("X", x);
+            anim.SetFloat("Y", y);
+        }
+
+        anim.SetBool("Moving", moving);
     }
 }
